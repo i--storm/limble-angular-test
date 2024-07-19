d4d9b1558cd2334f0788f2cc3a543cb0a3515aed
Vik <vyagnov@gmail.com>
2024-07-18 21:40:30 -0400

    caret position calculate

--- src/app/ta-user-tag/ta-user-tag.component.ts
+++ src/app/ta-user-tag/ta-user-tag.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+//const getCaretCoordinates = require('textarea-caret');
+import getCaretCoordinates from 'textarea-caret'
+
 
 @Component({
   selector: 'app-ta-user-tag',
@@ -11,14 +14,25 @@ import { CommonModule } from '@angular/common';
 export class TaUserTagComponent {
   @Input('users') users: any = [];
   is_show_dialog = false
+  dialog_top = "0px"
+  dialog_left = "0px"
+
 
   keyup(e: KeyboardEvent){
-    //let target = e.target as HTMLTextAreaElement;
+    let target = e.target as HTMLTextAreaElement;
 
     //if(target !== null && target.value==="@") {
     if(e.key === "@"){
-      console.log("keyup @")
+
+      let caret = getCaretCoordinates(target, target.selectionEnd);
+
+      console.log('(top, left, height) = (%s, %s, %s)', caret.top, caret.left, caret.height);
+
+      this.dialog_top = caret.top+"px";
+      this.dialog_left = caret.left+"px";
+
       this.is_show_dialog = true;
+
     }
   }
 }
